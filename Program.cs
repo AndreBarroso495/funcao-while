a6c10daed82f1214ca273fbb8d9287fa28c6f72a
FIC <FIC@info.local>
2020-11-17 10:53:36 -0300

    projeto concluido

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Funcao_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Aprendendo WHILE!");
+            
+            // Console.WriteLine("Digite um número");
+            // decimal numero = decimal.Parse(Console.ReadLine());
+
+            // decimal i = 0;
+
+            // Console.WriteLine("-------------------");
+
+            // //While repete quando a condição for veradeira
+            // while (i <= numero)
+            // {
+            //     Console.WriteLine(i);
+            //     i = i + 0.01m; // incremento
+            // } // Fim do While
+
+            // string resposta = "";
+            // do{
+            //     Console.WriteLine("Digite seu email");
+            //     resposta = Console.ReadLine();
+
+            // }while(resposta != "tsukam@gmail.com");
+
+            // Console.WriteLine("E-mail confirmado");
+
+            for(var contador = 0; contador < 10; contador++);
+
+
+
+
+
+        }
+    }
+}
